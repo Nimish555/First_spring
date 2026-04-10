d80dc443974a4066ba316277bcfafd3ad609c6d6
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-04-10 10:49:27 +0530

    done

--- src/main/java/com/example/loose/SMSService.java
+++ src/main/java/com/example/loose/SMSService.java
@@ -1,5 +1,9 @@
 package com.example.loose;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component("smsService")
 public class SMSService implements NoticationService{
 
     @Override
